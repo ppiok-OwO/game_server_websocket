937852736cc46b950dc80626dbabf34d3a332e63
ppiok-OwO <solcorin@naver.com>
2024-12-10 17:20:01 +0900

    🎉 add 데이터 베이스를 위한 json 세팅

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # 게임서버개발 심화주차 강의
 ## 웹소켓으로 실시간 게임 구현하기
+### 내일배움캠프 Node.js 7기
 내일배움캠프 <Node.js 게임서버개발> 강의를 듣고 실습을 해보는 레포지토리입니다.
+```
+game_server_websocket
+├─ package-lock.json
+├─ package.json
+├─ README.md
+└─ src
+   ├─ app.js
+   └─ init
+      └─ socket.js
+
+```
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { createServer } from 'http';
+import initSocket from './init/socket.js';
+
+const app = express();
+const server = createServer(app);
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+initSocket(server);
+
+app.get('/', (req, res) => {
+  // 테스트를 위한 API 생성
+  res.send('<h1>Hello World</h1>');
+});
+
+server.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+});
