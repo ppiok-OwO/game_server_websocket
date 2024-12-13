ad829756bfc5d2bb157e3d27d1d7c6177eb6c056
ppiok-OwO <solcorin@naver.com>
2024-12-13 12:48:33 +0900

    ✨ update 유저를 db로 관리

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import { loadGameAssets } from './init/asset.js';
 import path from 'path';
+import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 
 const app = express();
 const server = createServer(app);
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 initSocket(server);
+
+app.use(errorHandlingMiddleware);
 
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,45 @@
+import { Prisma } from '@prisma/client';
+import { prisma } from '../utils/prisma/index.js';
+
 // 유저 관련 CRUD 함수
 const users = []; // 접속 중인 유저의 배열
 
-export const addUser = (user) => {
-  users.push(user);
+export const checkUser = async (user) => {
+  try {
+    const isExistUser = await prisma.users.findFirst({
+      where: {
+        uuid: user.userId,
+      },
+    });
+
+    return isExistUser;
+  } catch (err) {
+    console.error(err.name);
+  }
 };
 
-export const removeUser = (socketId) => {
+export const addUser = async (user) => {
+  users.push(user);
+  try {
+    await prisma.users.create({
+      data: {
+        uuid: user.uuid,
+        socketId: user.socketId,
+      },
+    });
+  } catch (err) {
+    console.error(err.name);
+  }
+};
+
+export const removeUser = async (socketId) => {
   const index = users.findIndex((user) => user.socketId === socketId);
   if (index !== -1) {
+    try {
+    } catch (err) {
+      console.error(err.name);
+    }
+
     return users.splice(index, 1)[0];
   }
 };
